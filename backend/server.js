c2eb44b64c82d19cc77c4c46a826ba917fd730f0
sudharshansudhir <sudhir.ssr7@gmail.com>
2026-01-21 00:06:37 +0530

    fourteenth  commit:Deployment changes-After Changes

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
